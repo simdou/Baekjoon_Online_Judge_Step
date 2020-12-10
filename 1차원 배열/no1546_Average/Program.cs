d9510ee2b8485c783f59638a52da8dea06e15c89
simdou <emkang2048@gmail.com>
2020-12-11 04:07:57 +0900

    1차원 배열 추가

---
+++ 1차원 배열/no1546_Average/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace no1546_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float maxScore = 0;
+            int scoreCount = Convert.ToInt32(Console.ReadLine());
+            String[] scores = Console.ReadLine().Split(" ");
+            float fixScoreSum = 0;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if(maxScore < float.Parse(scores[i]))
+                {
+                    maxScore = float.Parse(scores[i]);
+                }
+            }
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                fixScoreSum += (float.Parse(scores[i]) / maxScore) * 100;
+            }
+
+            Console.WriteLine(fixScoreSum / scoreCount);
+        }
+    }
+}
---
+++ 1차원 배열/no3052_Remainder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace no3052_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            int count = 0;
+            bool flag = true;
+
+            for(int i = 0; i < 10; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine()) % 42;
+            }
+
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    flag = true;
+
+                    if (i == j)
+                        continue;
+
+                    if (nums[i] == nums[j])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag)
+                    count += 1;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
