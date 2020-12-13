38af341b249a2652694001cb765a35c4ac7b7421
simdou <emkang2048@gmail.com>
2020-12-14 05:56:37 +0900

    1차원 배열 최종

---
+++ 1차원 배열/no4344_above_Average/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace no4344_above_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                float average = 0;
+                float result = 0;
+                int overCount = 0;
+                string[] scores = Console.ReadLine().Split(" ");
+
+                for(int j = 1; j <= int.Parse(scores[0]); j++)
+                {
+                    average += int.Parse(scores[j]);
+                }
+
+                average = (average / int.Parse(scores[0]));
+
+                for (int j = 1; j <= int.Parse(scores[0]); j++)
+                {
+                    if(float.Parse(scores[j]) > average)
+                    {
+                        overCount++;
+                    }
+                }
+
+                result = overCount / float.Parse(scores[0]);
+
+                Console.WriteLine("{0:0.000}" + "%", result * 100);
+            }
+        }
+    }
+}
---
+++ 1차원 배열/no8958_OX_Quiz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace no8958_OX_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] count = new int[int.Parse(Console.ReadLine())];
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                string OX = Console.ReadLine();
+                int score = 0;
+                int resurt = 0;
+
+                for (int j = 0; j < OX.Length; j++)
+                {
+                    if (OX[j] == 'O')
+                    {
+                        score += 1;
+                        resurt += score;
+                    }
+                    else
+                    {
+                        score = 0;
+                    }
+                }
+
+                Console.WriteLine(resurt);
+            }
+        }
+    }
+}
