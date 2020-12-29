7adc244b5d8503e1561e935d066e82b002e23458
simdou <emkang2048@gmail.com>
2020-12-30 01:58:45 +0900

    함수 문제 추가

---
+++ 함수/no4673_Self_Number/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace no4673_Self_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] nums = new bool[10001];
+            int num = 0;
+
+            while (num <= 10000)
+            {
+                try
+                {
+                    nums[Df(num.ToString())] = true;
+                    num++;
+                }
+                catch (Exception)
+                {
+                    num++;
+                    continue;
+                }
+            }
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == false)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static int Df(string num)
+        {
+            int result = int.Parse(num);
+
+            for(int i = 0; i < num.Length; i++)
+            {
+                result = result + int.Parse(num[i].ToString());
+            }
+
+            return result;
+        }
+    }
+}
