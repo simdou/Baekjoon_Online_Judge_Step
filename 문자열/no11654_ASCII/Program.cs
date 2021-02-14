f80b10dba8987eee0d2a812d4036a3d4e980a0d9
simdou <emkang2048@gmail.com>
2021-02-15 03:06:45 +0900

    문자열1

---
+++ 문자열/no11654_ASCII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace no11654_ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert.ToInt32(Convert.ToChar(Console.ReadLine())));
+        }
+    }
+}
---
+++ 문자열/no11720_Numbers_Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace no11720_Numbers_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int count = Convert.ToInt32(Console.ReadLine());
+            char[] nums = Console.ReadLine().ToCharArray();
+
+            for(int i = 0; i < count; i++)
+            {
+                result += Convert.ToInt32(Convert.ToString(nums[i]));
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
